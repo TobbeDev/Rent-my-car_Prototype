3f8c3124695e8ce5f14d496cb6e1e7a88a77ef84
RasmusHorntvedt <rasmus.horntvedt0004@stud.hkr.se>
2018-11-02 14:34:48 +0100

    made ad page and search page

---
+++ app/src/main/java/com/example/group/entrepreneurship_prototype/AddLayoutActivity.java
@@ -0,0 +1,13 @@
+package com.example.group.entrepreneurship_prototype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_layout);
+    }
+}
